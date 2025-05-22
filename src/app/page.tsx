8472b92243775223df29e607625535a00141d9dd
rpawe <115289748+wegorz9@users.noreply.github.com>
2025-05-22 19:15:12 +0200

    fixed app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import Navbar from "~/app/_components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getServerSession } from "next-auth";
+import { authOptions } from "~/server/auth";
 // import ApartmentCard from "~/components/ApartmentCard";
 // coś ne tak z tym
 // import 'bootstrap/dist/js/bootstrapped.js';
 
 
 
-export default function Home() {
-    return (
-        <>
-          <Navbar />
-
-
-        </>
-    );
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  const isLoggedIn = !!session?.user;
+  return (
+    <>
+      <Navbar isLoggedIn={isLoggedIn} user={session?.user} />
+    </>
+  );
 }
